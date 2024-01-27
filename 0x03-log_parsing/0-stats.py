b0f4ecb91dc4bc95e47d5fba070add1a2e878781
Noran S Abdelfattah <noransaber685@gmail.com>
2024-01-27 06:22:24 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,40 +3,70 @@
 This module contains the function that displays the
 stats from the standard input
 """
-import re
 import sys
+import re
 
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
-print_counter = 0
-size_summation = 0
+def initialize_log():
+  """
+  This function initializes a log dictionary with keys for file size
+  and a dictionary of HTTP status codes. The initial values are set
+  to 0, and the function 
+  """
+    status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    log = {"file_size": 0, "code_list": {str(code): 0 for code in status_codes}}
+    print(f"{log = }")
+    return log
 
+def parse_line(line, regex, log):
+  """
+  This function takes a log line, a regular expression (regex), and the
+  current log dictionary as input. It attempts to match the log line using the regular
+  expression and updates the log with relevant information such as file size and HTTP status
+  codes.
+  """
+    match = regex.fullmatch(line)
 
-def print_logs():
-    """
-    Prints status codes to the logs
-    """
-    print("File size: {}".format(size_summation))
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
+    if match:
+        stat_code, file_size = match.group(1, 2)
+        log["file_size"] += int(file_size)
+        if stat_code.isdecimal():
+            log["code_list"][stat_code] += 1
+    return log
 
+def print_codes(log):
+  """
+  This function takes the log dictionary as input and prints the file size and
+  counts of each HTTP status code that occurred in the log. It sorts the status
+  codes before printing for better readability.
+  """
+    print("File size: {}".format(log["file_size"]))
+
+    sorted_code_list = sorted(log["code_list"])
+    for code in sorted_code_list:
+        if log["code_list"][code]:
+            print(f"{code}: {log['code_list'][code]}")
+
+def main():
+  """
+  The main function of the script. It initializes a regular expression
+  for parsing log lines, initializes the log, and then reads log lines from
+  standard input. It calls the parse_line function to update the log for
+  every log line and prints log statistics every 10 lines.
+  """
+    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
+
+    log = initialize_log()
+    line_count = 0
+
+    for line in sys.stdin:
+        line = line.strip()
+        line_count += 1
+        log = parse_line(line, regex, log)
+
+        if line_count % 10 == 0:
+            print_codes(log)
 
 if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            std_line = line.strip()
-            log_list = re.split('- | "|" | " " ', std_line)
-            try:
-                codes = log_list[-1].split(" ")
-                if len(codes) == 2 and int(codes[0]) in status_codes:
-                    status_codes[int(codes[0])] += 1
-                    print_counter += 1
-                    size_summation += int(codes[1])
-                    if print_counter % 10 == 0:
-                        print_logs()
-            except (ValueError, IndexError):
-                pass
-        print_logs()
-    except KeyboardInterrupt:
-        print_logs()
+    main()
+    initialize_log()
+
